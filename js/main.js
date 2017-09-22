d298a1ffb7f5c1401eb537eb3fb535794ac716b0
dmnmkua <dmn.mk.ua@gmail.com>
2017-09-22 21:00:18 +0300

    finish main page

---
+++ js/main.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+  var wrapper = document.querySelector('#wrapper');
+  var nav = document.querySelector('#nav');
+  var btnMenu = document.querySelector('#btn-menu');
+  var btnClose = document.querySelector('#btn-close')
+
+  btnMenu.addEventListener('click', function() {
+    nav.classList.toggle('nav--close');
+    wrapper.classList.toggle('wrapper--close');
+  })
+
+  btnClose.addEventListener('click', function() {
+    nav.classList.toggle('nav--close');
+    wrapper.classList.toggle('wrapper--close');
+  })
+}
