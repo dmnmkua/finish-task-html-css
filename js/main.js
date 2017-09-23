6319f276e4ffc0bef229f527c63ec0dbcf18edc2
dmnmkua <dmn.mk.ua@gmail.com>
2017-09-23 12:16:04 +0300

    repair menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-window.onload = function() {
+window.addEventListener('DOMContentLoaded' , init_);
+
+function init_() {
   var wrapper = document.querySelector('#wrapper');
   var nav = document.querySelector('#nav');
   var btnMenu = document.querySelector('#btn-menu');
